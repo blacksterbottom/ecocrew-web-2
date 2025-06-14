2dfa6874ce1c89869321c12019a56abb4a809469
backsterbottom <nattapol.cx@gmail.com>
2025-06-14 09:41:58 +0700

    add GA4

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+// import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import SeenUsOn from './components/SeenUsOn';
 import HowItWorks from './components/HowItWorks';
 import Benefits from './components/Benefits';
 import Products from './components/Products';
-import Testimonials from './components/Testimonials';
+// import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
